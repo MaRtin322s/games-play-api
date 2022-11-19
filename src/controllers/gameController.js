93a7689b358fdd2d324efb1063b9789ebab031e0
MaRtin322s <117728421+MaRtin322s@users.noreply.github.com>
2022-11-19 14:59:27 +0200

    edit game

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -16,14 +16,26 @@ router.get('/games?sortBy=_createdOn%20desc&distinct=category', async (req, res)
 router.get('/games/:gameId', async (req, res) => {
     const gameId = req.params.gameId;
     const game = await gameService.getOne(gameId);
+    game['_ownerId'] = game._ownerId.toString();
     res.json(game);
 });
 
 router.post('/games', async (req, res) => {
     const { title, category, maxLevel, imageUrl, summary } = req.body;
     const owner = req.user;
-    const game = await gameService.createGame({ title, category, maxLevel, imageUrl, summary, owner });
+    const game = await gameService.createGame({ title, category, maxLevel, imageUrl, summary, _ownerId: owner });
     res.json(game);
+});
+
+router.put('/games/:gameId', async (req, res) => {
+    const gameId = req.params.gameId;
+    const gameData = req.body;
+    const game = await gameService.editGame(gameId, gameData);
+    res.json(game);
+});
+
+router.delete('/games/:gameId', async (req, res) => {
+
 });
 
 module.exports = router;
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -4,3 +4,4 @@ exports.getAll = async () => await Game.find().lean();
 exports.createGame = async (gameData) => await Game.create(gameData);
 exports.lastGames = async () => await Game.find().sort({ createdAt: 1 }).limit(3).lean();
 exports.getOne = async (gameId) => await Game.findById({ _id: gameId }).lean();
+exports.editGame = async (gameId, gameData) => await Game.findByIdAndUpdate({ _id: gameId}, gameData);
